638dea5ab9e1cdeff73f148b70ad8eb9cf7c5217
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2026-01-13 12:21:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ app.use(
 );
 
 // ---------- Body Parsing ----------
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 // ---------- Static Files ----------
@@ -77,3 +77,4 @@ app.use(globalErrorHandler);
 
 
 export default app;
+
